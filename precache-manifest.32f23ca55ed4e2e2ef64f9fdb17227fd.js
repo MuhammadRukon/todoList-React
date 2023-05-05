414d1f4debcef9339dcc31fc7f2d349ee69259e6
Muhammad.rukon <muhammad.rukon619@gmail.com>
2023-05-05 07:52:41 +0600

    Updates

---
+++ precache-manifest.32f23ca55ed4e2e2ef64f9fdb17227fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f3f800672f52b9f37700b898799e2f",
+    "url": "/todoList-react/index.html"
+  },
+  {
+    "revision": "9d4f9fd5393c0017b201",
+    "url": "/todoList-react/static/js/2.a43caebe.chunk.js"
+  },
+  {
+    "revision": "e28096ee471bb6a15a3d",
+    "url": "/todoList-react/static/js/main.e87f588f.chunk.js"
+  },
+  {
+    "revision": "0996d26920bbba6cedb9",
+    "url": "/todoList-react/static/js/runtime-main.b7125d64.js"
+  }
+]);
